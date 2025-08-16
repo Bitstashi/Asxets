c0d29c39cf7fbedb3dc42742cbd65bd31178662d
Bitstashi <marsoverlord05@gmail.com>
2025-08-16 23:44:04 +0530

    Delete deploy directory

--- deploy/deploy/01-deploy.js
+++
@@ -1,7 +0,0 @@
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  const Token = await ethers.getContractFactory("RWAInfrastructureToken");
-  const token = await Token.deploy();
-  console.log("Deployed to:", token.address);
-}
-main().catch(console.error);
